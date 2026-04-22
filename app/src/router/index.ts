57bc141dc9dfb001f11fd62f219ddfe93c1dab86
JamesHagerty1 <jhagerty@alumni.cmu.edu>
2026-04-22 10:56:36 -0400

    code styling will use tabs

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,15 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import LandingView from '@/views/LandingView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import LandingView from "@/views/LandingView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'landing',
-      component: LandingView,
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+      	{
+        	path: "/",
+      		name: "landing",
+      		component: LandingView,
+   		},
+  	],
 });
 
 export default router;
